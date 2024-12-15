2c6c0939434ebcde2adf10dc690b6e2322691cf5
unknown <brunoburguesadf@gmail.com>
2024-12-15 19:46:53 -0300

    Agregado de visual

--- tpwinfom-grupo-i/tpwinfom-grupo-i/Program.cs
+++ tpwinfom-grupo-i/tpwinfom-grupo-i/Program.cs
@@ -16,7 +16,7 @@ namespace tpwinfom_grupo_i
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new menuPrincipalVentana());
         }
     }
 }
---
+++ tpwinfom-grupo-i/tpwinfom-grupo-i/marcaClase.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tpwinfom_grupo_i
+{
+    internal class marcaClase
+    {
+    }
+}

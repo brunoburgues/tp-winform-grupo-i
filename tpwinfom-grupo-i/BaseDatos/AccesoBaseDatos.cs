41e029df3c517cd2188bf0ee71e9f1c3f8312395
Diego Ramirez Almaguer <diegoramalm456@gmail.com>
2024-12-16 20:01:37 -0600

    conexion a db

---
+++ tpwinfom-grupo-i/BaseDatos/AccesoBaseDatos.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace BaseDatos
+{
+    public class AccesoBaseDatos
+    {
+        private SqlConnection conexion;
+        private SqlCommand comando;
+        private SqlDataReader reader;
+
+        public AccesoBaseDatos() {
+            conexion = new SqlConnection("server=.\\SQLEXPRESS; database=CATALOGO_P3_DB; integrated security=true");
+            comando = new SqlCommand();
+        }
+
+    }
+}

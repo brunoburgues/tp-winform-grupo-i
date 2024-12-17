c3114c1e901508b1b8f164956f538f8fa0870949
Diego Ramirez Almaguer <diegoramalm456@gmail.com>
2024-12-16 20:18:49 -0600

    Conexion a db

--- tpwinfom-grupo-i/BaseDatos/AccesoBaseDatos.cs
+++ tpwinfom-grupo-i/BaseDatos/AccesoBaseDatos.cs
@@ -13,10 +13,41 @@ namespace BaseDatos
         private SqlCommand comando;
         private SqlDataReader reader;
 
-        public AccesoBaseDatos() {
+        public SqlDataReader Reader { get { return reader; } }
+
+        public AccesoBaseDatos() 
+        {
             conexion = new SqlConnection("server=.\\SQLEXPRESS; database=CATALOGO_P3_DB; integrated security=true");
             comando = new SqlCommand();
         }
 
+        public void SetConsulta(string consulta)
+        {
+            comando.CommandType = System.Data.CommandType.Text;
+            comando.CommandText = consulta;
+        }
+
+        public void Lectura()
+        {
+            comando.Connection =  conexion;
+            try
+            {
+                conexion.Open();
+                reader = comando.ExecuteReader();
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+
+        }
+
+        public void CloseConexion()
+        {
+            if (reader != null) 
+                reader.Close();
+            conexion.Close();
+        }
     }
 }

c073e44c6e561a50730ef3d7d8fc2be43b7d6f3e
Diego Ramirez Almaguer <diegoramalm456@gmail.com>
2024-12-21 11:54:26 -0600

    Agregar y modificar arreglado

--- tpwinfom-grupo-i/tpwinfom-grupo-i/ArtículoVentana.Designer.cs
+++ tpwinfom-grupo-i/tpwinfom-grupo-i/ArtículoVentana.Designer.cs
@@ -39,12 +39,13 @@
             this.imagenes = new System.Windows.Forms.Button();
             this.agregar = new System.Windows.Forms.Button();
             this.cancelar = new System.Windows.Forms.Button();
-            this.cajaCódigo = new System.Windows.Forms.TextBox();
             this.cajaNombre = new System.Windows.Forms.TextBox();
             this.cajaPrecio = new System.Windows.Forms.TextBox();
             this.eliminar = new System.Windows.Forms.Button();
-            this.listaCategoría = new System.Windows.Forms.ListBox();
-            this.listaMarca = new System.Windows.Forms.ListBox();
+            this.cbMarca = new System.Windows.Forms.ComboBox();
+            this.cbCategoria = new System.Windows.Forms.ComboBox();
+            this.lblDescripcion = new System.Windows.Forms.Label();
+            this.tbCodigo = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxImagenes)).BeginInit();
             this.SuspendLayout();
             // 
@@ -53,10 +54,9 @@
             this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(106, 20);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Location = new System.Drawing.Point(71, 13);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(90, 20);
+            this.label1.Size = new System.Drawing.Size(61, 13);
             this.label1.TabIndex = 0;
             this.label1.Text = "ARTICULO";
             // 
@@ -65,34 +65,31 @@
             this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(33, 72);
-            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Location = new System.Drawing.Point(22, 47);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(136, 20);
+            this.label2.Size = new System.Drawing.Size(97, 13);
             this.label2.TabIndex = 1;
-            this.label2.Text = "Código de artículo";
+            this.label2.Text = "Código de artículo:";
             // 
             // label3
             // 
             this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(33, 112);
-            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Location = new System.Drawing.Point(22, 73);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(65, 20);
+            this.label3.Size = new System.Drawing.Size(47, 13);
             this.label3.TabIndex = 2;
-            this.label3.Text = "Nombre";
+            this.label3.Text = "Nombre:";
             // 
             // pictureBoxImagenes
             // 
             this.pictureBoxImagenes.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.pictureBoxImagenes.Location = new System.Drawing.Point(304, 20);
-            this.pictureBoxImagenes.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.pictureBoxImagenes.Location = new System.Drawing.Point(203, 13);
             this.pictureBoxImagenes.Name = "pictureBoxImagenes";
-            this.pictureBoxImagenes.Size = new System.Drawing.Size(279, 263);
+            this.pictureBoxImagenes.Size = new System.Drawing.Size(186, 171);
             this.pictureBoxImagenes.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBoxImagenes.TabIndex = 3;
             this.pictureBoxImagenes.TabStop = false;
@@ -101,58 +98,52 @@
             // 
             this.cajaDescripcion.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left)));
-            this.cajaDescripcion.ForeColor = System.Drawing.Color.Gray;
-            this.cajaDescripcion.Location = new System.Drawing.Point(38, 285);
-            this.cajaDescripcion.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.cajaDescripcion.ForeColor = System.Drawing.Color.Black;
+            this.cajaDescripcion.Location = new System.Drawing.Point(25, 191);
             this.cajaDescripcion.Multiline = true;
             this.cajaDescripcion.Name = "cajaDescripcion";
-            this.cajaDescripcion.Size = new System.Drawing.Size(246, 115);
+            this.cajaDescripcion.Size = new System.Drawing.Size(165, 70);
             this.cajaDescripcion.TabIndex = 4;
-            this.cajaDescripcion.Text = "Descripcion...";
             // 
             // marca
             // 
             this.marca.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.marca.AutoSize = true;
-            this.marca.Location = new System.Drawing.Point(33, 158);
-            this.marca.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.marca.Location = new System.Drawing.Point(22, 103);
             this.marca.Name = "marca";
-            this.marca.Size = new System.Drawing.Size(53, 20);
+            this.marca.Size = new System.Drawing.Size(40, 13);
             this.marca.TabIndex = 5;
-            this.marca.Text = "Marca";
+            this.marca.Text = "Marca:";
             // 
             // Categoría
             // 
             this.Categoría.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.Categoría.AutoSize = true;
-            this.Categoría.Location = new System.Drawing.Point(33, 192);
-            this.Categoría.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.Categoría.Location = new System.Drawing.Point(22, 125);
             this.Categoría.Name = "Categoría";
-            this.Categoría.Size = new System.Drawing.Size(78, 20);
+            this.Categoría.Size = new System.Drawing.Size(57, 13);
             this.Categoría.TabIndex = 6;
-            this.Categoría.Text = "Categoría";
+            this.Categoría.Text = "Categoría:";
             // 
             // label6
             // 
             this.label6.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(33, 232);
-            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Location = new System.Drawing.Point(22, 151);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(53, 20);
+            this.label6.Size = new System.Drawing.Size(40, 13);
             this.label6.TabIndex = 7;
-            this.label6.Text = "Precio";
+            this.label6.Text = "Precio:";
             // 
             // imagenes
             // 
             this.imagenes.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-            this.imagenes.Location = new System.Drawing.Point(304, 303);
-            this.imagenes.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.imagenes.Location = new System.Drawing.Point(203, 197);
             this.imagenes.Name = "imagenes";
-            this.imagenes.Size = new System.Drawing.Size(135, 35);
+            this.imagenes.Size = new System.Drawing.Size(90, 23);
             this.imagenes.TabIndex = 8;
             this.imagenes.Text = "Imágenes";
             this.imagenes.UseVisualStyleBackColor = true;
@@ -161,10 +152,9 @@
             // agregar
             // 
             this.agregar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-            this.agregar.Location = new System.Drawing.Point(448, 303);
-            this.agregar.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.agregar.Location = new System.Drawing.Point(299, 197);
             this.agregar.Name = "agregar";
-            this.agregar.Size = new System.Drawing.Size(135, 35);
+            this.agregar.Size = new System.Drawing.Size(90, 23);
             this.agregar.TabIndex = 9;
             this.agregar.Text = "Agregar";
             this.agregar.UseVisualStyleBackColor = true;
@@ -177,85 +167,86 @@
             this.cancelar.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.cancelar.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.cancelar.ForeColor = System.Drawing.SystemColors.InactiveBorder;
-            this.cancelar.Location = new System.Drawing.Point(448, 362);
-            this.cancelar.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.cancelar.Location = new System.Drawing.Point(299, 235);
             this.cancelar.Name = "cancelar";
-            this.cancelar.Size = new System.Drawing.Size(135, 35);
+            this.cancelar.Size = new System.Drawing.Size(90, 23);
             this.cancelar.TabIndex = 10;
             this.cancelar.Text = "Cancelar";
             this.cancelar.UseVisualStyleBackColor = false;
             this.cancelar.Click += new System.EventHandler(this.cancelar_Click);
             // 
-            // cajaCódigo
-            // 
-            this.cajaCódigo.Location = new System.Drawing.Point(198, 68);
-            this.cajaCódigo.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.cajaCódigo.Name = "cajaCódigo";
-            this.cajaCódigo.Size = new System.Drawing.Size(85, 26);
-            this.cajaCódigo.TabIndex = 13;
-            // 
             // cajaNombre
             // 
-            this.cajaNombre.Location = new System.Drawing.Point(123, 108);
-            this.cajaNombre.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.cajaNombre.Location = new System.Drawing.Point(82, 70);
             this.cajaNombre.Name = "cajaNombre";
-            this.cajaNombre.Size = new System.Drawing.Size(160, 26);
+            this.cajaNombre.Size = new System.Drawing.Size(108, 20);
             this.cajaNombre.TabIndex = 14;
             // 
             // cajaPrecio
             // 
-            this.cajaPrecio.Location = new System.Drawing.Point(123, 229);
-            this.cajaPrecio.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.cajaPrecio.Location = new System.Drawing.Point(82, 149);
             this.cajaPrecio.Name = "cajaPrecio";
-            this.cajaPrecio.Size = new System.Drawing.Size(160, 26);
+            this.cajaPrecio.Size = new System.Drawing.Size(108, 20);
             this.cajaPrecio.TabIndex = 15;
             // 
             // eliminar
             // 
             this.eliminar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-            this.eliminar.Location = new System.Drawing.Point(304, 362);
-            this.eliminar.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.eliminar.Location = new System.Drawing.Point(203, 235);
             this.eliminar.Name = "eliminar";
-            this.eliminar.Size = new System.Drawing.Size(135, 35);
+            this.eliminar.Size = new System.Drawing.Size(90, 23);
             this.eliminar.TabIndex = 16;
             this.eliminar.Text = "Eliminar";
             this.eliminar.UseVisualStyleBackColor = true;
             this.eliminar.Visible = false;
             this.eliminar.Click += new System.EventHandler(this.eliminar_Click);
             // 
-            // listaCategoría
+            // cbMarca
             // 
-            this.listaCategoría.FormattingEnabled = true;
-            this.listaCategoría.ItemHeight = 20;
-            this.listaCategoría.Location = new System.Drawing.Point(123, 197);
-            this.listaCategoría.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.listaCategoría.Name = "listaCategoría";
-            this.listaCategoría.Size = new System.Drawing.Size(160, 24);
-            this.listaCategoría.TabIndex = 12;
-            this.listaCategoría.SelectedIndexChanged += new System.EventHandler(this.listaCategoría_SelectedIndexChanged);
+            this.cbMarca.FormattingEnabled = true;
+            this.cbMarca.Location = new System.Drawing.Point(76, 98);
+            this.cbMarca.Name = "cbMarca";
+            this.cbMarca.Size = new System.Drawing.Size(121, 21);
+            this.cbMarca.TabIndex = 17;
             // 
-            // listaMarca
+            // cbCategoria
             // 
-            this.listaMarca.FormattingEnabled = true;
-            this.listaMarca.ItemHeight = 20;
-            this.listaMarca.Location = new System.Drawing.Point(123, 149);
-            this.listaMarca.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.listaMarca.Name = "listaMarca";
-            this.listaMarca.Size = new System.Drawing.Size(160, 24);
-            this.listaMarca.TabIndex = 11;
-            this.listaMarca.SelectedIndexChanged += new System.EventHandler(this.listaMarca_SelectedIndexChanged);
+            this.cbCategoria.FormattingEnabled = true;
+            this.cbCategoria.Location = new System.Drawing.Point(76, 122);
+            this.cbCategoria.Name = "cbCategoria";
+            this.cbCategoria.Size = new System.Drawing.Size(121, 21);
+            this.cbCategoria.TabIndex = 18;
+            // 
+            // lblDescripcion
+            // 
+            this.lblDescripcion.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblDescripcion.AutoSize = true;
+            this.lblDescripcion.Location = new System.Drawing.Point(22, 175);
+            this.lblDescripcion.Name = "lblDescripcion";
+            this.lblDescripcion.Size = new System.Drawing.Size(66, 13);
+            this.lblDescripcion.TabIndex = 19;
+            this.lblDescripcion.Text = "Descripcion:";
+            // 
+            // tbCodigo
+            // 
+            this.tbCodigo.Location = new System.Drawing.Point(125, 44);
+            this.tbCodigo.Name = "tbCodigo";
+            this.tbCodigo.Size = new System.Drawing.Size(65, 20);
+            this.tbCodigo.TabIndex = 20;
             // 
             // ArtículoVentana
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(621, 417);
+            this.ClientSize = new System.Drawing.Size(414, 271);
+            this.Controls.Add(this.tbCodigo);
+            this.Controls.Add(this.lblDescripcion);
+            this.Controls.Add(this.cbCategoria);
+            this.Controls.Add(this.cbMarca);
             this.Controls.Add(this.eliminar);
             this.Controls.Add(this.cajaPrecio);
             this.Controls.Add(this.cajaNombre);
-            this.Controls.Add(this.cajaCódigo);
-            this.Controls.Add(this.listaCategoría);
-            this.Controls.Add(this.listaMarca);
             this.Controls.Add(this.cancelar);
             this.Controls.Add(this.agregar);
             this.Controls.Add(this.imagenes);
@@ -267,8 +258,7 @@
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.MinimumSize = new System.Drawing.Size(634, 447);
+            this.MinimumSize = new System.Drawing.Size(428, 304);
             this.Name = "ArtículoVentana";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Agregar Artículo";
@@ -292,11 +282,12 @@
         private System.Windows.Forms.Button imagenes;
         private System.Windows.Forms.Button agregar;
         private System.Windows.Forms.Button cancelar;
-        private System.Windows.Forms.TextBox cajaCódigo;
         private System.Windows.Forms.TextBox cajaNombre;
         private System.Windows.Forms.TextBox cajaPrecio;
         private System.Windows.Forms.Button eliminar;
-        private System.Windows.Forms.ListBox listaCategoría;
-        private System.Windows.Forms.ListBox listaMarca;
+        private System.Windows.Forms.ComboBox cbMarca;
+        private System.Windows.Forms.ComboBox cbCategoria;
+        private System.Windows.Forms.Label lblDescripcion;
+        private System.Windows.Forms.TextBox tbCodigo;
     }
 }
--- tpwinfom-grupo-i/tpwinfom-grupo-i/ArtículoVentana.cs
+++ tpwinfom-grupo-i/tpwinfom-grupo-i/ArtículoVentana.cs
@@ -51,14 +51,7 @@ namespace tpwinfom_grupo_i
             }
             return listaImagenes;
         }
-        private bool modificarArticulo()
-        {
-            if (articulo == null)
-            {
-                return false;
-            }
-            return true;
-        }
+
         private void cargarImagen(string url)
         {
             try
@@ -74,68 +67,67 @@ namespace tpwinfom_grupo_i
         private void ArtículoVentana_Load(object sender, EventArgs e)
         {
             CategoriaDB categoriaDB = new CategoriaDB();
-            listaCategoría.DataSource = categoriaDB.listarCategoria();
-            listaCategoría.DisplayMember = "Nombre";
-            listaCategoría.ValueMember = "Id";
             MarcaDB marcaDB = new MarcaDB();
-            listaMarca.DataSource = marcaDB.listarMarcas();
-            listaMarca.DisplayMember = "Nombre";
-            listaMarca.ValueMember = "Id";
-            if (articulo == null)
+            try
             {
-                pictureBoxImagenes.Load("");
-                
-                listaCategoría.SelectedIndex = -1;
-                listaMarca.SelectedIndex = -1;
+                cbCategoria.DataSource = categoriaDB.listarCategoria();
+                cbCategoria.ValueMember = "Id";
+                cbCategoria.DisplayMember = "Nombre";
+                cbMarca.DataSource = marcaDB.listarMarcas();
+                cbMarca.ValueMember = "Id";
+                cbMarca.DisplayMember = "Nombre";
+                if (articulo == null)
+                {
+                    pictureBoxImagenes.Load("https://www.pngkey.com/png/full/233-2332677_ega-png.png");
+                    cbCategoria.SelectedIndex = -1;
+                    cbMarca.SelectedIndex = -1;
+                }
+                else
+                {
+                    tbCodigo.Text = articulo.Codigo;
+                    cajaNombre.Text = articulo.Nombre;
+                    cajaPrecio.Text = $"{articulo.Precio}";
+                    cajaDescripcion.Text = articulo.Descripcion;
+                    cbCategoria.SelectedValue = articulo.Categoria.Id;
+                    cbMarca.SelectedValue = articulo.Marca.Id;
+                    listarImagenes();
+                    listaImagenes = new List<string>();
+                    extraerUrl();
+                    //Muestra la primera imágen como previsualizacion.
+                    cargarImagen(listaImagenes[0]);
+                }
             }
-            else
+            catch (Exception ex)
             {
-                cajaCódigo.Text = articulo.Codigo;
-                cajaNombre.Text = articulo.Nombre;
-                cajaPrecio.Text = $"{articulo.Precio}";
-                cajaDescripcion.Text = articulo.Descripcion;
-                listaCategoría.SelectedItem = articulo.Categoria.Id;
-                listaMarca.SelectedItem = articulo.Marca.Id;
-                listarImagenes();
-                listaImagenes = new List<string>();
-                extraerUrl();
-                //Muestra la primera imágen como previsualizacion.
-                cargarImagen(listaImagenes[0]);
-            }           
-        }
 
+                throw ex;
+            }          
+        }
         private void agregar_Click(object sender, EventArgs e)
         {
             ArticuloDB articuloDB = new ArticuloDB();
             ImagenDB imagenesDB = new ImagenDB();
-            
             try
             {
-                articulo.Codigo = cajaCódigo.Text;
-                articulo.Nombre = cajaNombre.Text;
-                articulo.Precio = decimal.Parse(cajaPrecio.Text);
-                articulo.Marca = (Marca)listaMarca.SelectedItem;
-                articulo.Categoria = (Categoria)listaCategoría.SelectedItem;
-                articulo.Descripcion = cajaDescripcion.Text;
-
-                if (modificarArticulo())
+                if (articulo == null)
                 {
+                    establecerArticulo();
                     articuloDB.modificar(articulo);
                     //Las imágenes se Eliminan y Agregan en la BD en la Ventana Galeria
-                    MessageBox.Show("Modificación Exitosa.");
+                    MessageBox.Show("Se modifico exitosamente.");
                 }
                 else
                 {
+                    articulo = new Articulo();
+                    establecerArticulo();
                     articuloDB.agregar(articulo);
-
                     articulo = articuloDB.TraerUltimoArticulo();
                     //Guarda una imagen por vez
                     foreach (string imagen in listaImagenes)
                     {
                         imagenesDB.AgregarImagen(articulo.Id, imagen);
                     }
-
-                    MessageBox.Show("Agregado exitoso");
+                    MessageBox.Show("Se agrego exitosamente");
                 }
             }
             catch (Exception ex)
@@ -145,13 +137,10 @@ namespace tpwinfom_grupo_i
             }
             finally { Close(); }
         }
-
         private void cancelar_Click(object sender, EventArgs e)
         {
             Close();
         }
-
-
         private void galeria_Click(object sender, EventArgs e)
         {
             Galeria ventanaGaleria;
@@ -179,33 +168,30 @@ namespace tpwinfom_grupo_i
             extraerUrl();
             pictureBoxImagenes.Load(listaImagenes[0]);
         }
-
         private void listaMarca_SelectedIndexChanged(object sender, EventArgs e)
         {
             if (!modificarArticulo())
             {
-                Marca selectedItem = (Marca)listaMarca.SelectedItem;
+                Marca selectedItem = (Marca)cbMarca.SelectedItem;
 
                 if (selectedItem != null)
                 {
-                    listaMarca.SelectedItem = listaMarca.Items[selectedItem.Id];
+                    cbMarca.SelectedItem = cbMarca.Items[selectedItem.Id];
                 }
             }
         }
-
         private void listaCategoría_SelectedIndexChanged(object sender, EventArgs e)
         {
             if (!modificarArticulo())
             {
-                Categoria selectedItem = (Categoria)listaCategoría.SelectedItem;
+                Categoria selectedItem = (Categoria)cbCategoria.SelectedItem;
 
                 if (selectedItem != null)
                 {
-                    listaCategoría.SelectedItem = listaCategoría.Items[selectedItem.Id];
+                    cbCategoria.SelectedItem = cbCategoria.Items[selectedItem.Id];
                 }
             }
         }
-
         private void eliminar_Click(object sender, EventArgs e)
         {
             ArticuloDB nuevo = new ArticuloDB();
@@ -221,6 +207,15 @@ namespace tpwinfom_grupo_i
                
                 throw;
             }
+        }
+        private void establecerArticulo()
+        {
+            articulo.Codigo = tbCodigo.Text;
+            articulo.Nombre = cajaNombre.Text;
+            articulo.Precio = decimal.Parse(cajaPrecio.Text);
+            articulo.Marca = (Marca)cbMarca.SelectedItem;
+            articulo.Categoria = (Categoria)cbCategoria.SelectedItem;
+            articulo.Descripcion = cajaDescripcion.Text;
         }
     }
 }
